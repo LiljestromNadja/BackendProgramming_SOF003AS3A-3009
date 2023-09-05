055244ed198d9376592853aa67782a127ea1218d
Nadja Liljeström <nliljestrom@gmail.com>
2023-09-05 22:03:39 +0300

    week2

---
+++ week2/src/main/java/hhBackEnd/week2/domain/Student.java
@@ -0,0 +1,39 @@
+package hhBackEnd.week2.domain;
+
+//teht 2
+public class Student {
+	
+	private String nimi, osoite;
+	
+	
+
+	public Student(String nimi, String osoite) {
+		super();
+		this.nimi = nimi;
+		this.osoite = osoite;
+	}
+
+	public String getNimi() {
+		return nimi;
+	}
+
+	public void setNimi(String nimi) {
+		this.nimi = nimi;
+	}
+
+	public String getOsoite() {
+		return osoite;
+	}
+
+	public void setOsoite(String osoite) {
+		this.osoite = osoite;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [nimi=" + nimi + ", osoite=" + osoite + "]";
+	}
+	
+	
+
+}
