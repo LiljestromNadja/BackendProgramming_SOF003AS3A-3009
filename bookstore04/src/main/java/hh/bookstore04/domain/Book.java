6baed4870bc4147f24aa973aaf64e683541a0cd1
Nadja Liljeström <nliljestrom@gmail.com>
2023-10-04 16:19:04 +0300

    week 6

---
+++ bookstore04/src/main/java/hh/bookstore04/domain/Book.java
@@ -0,0 +1,111 @@
+package hh.bookstore04.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Book {
+	
+	@Id
+	//@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Size(min = 1, max = 40)
+	private String title, author, isbn;
+	private int publicationYear;
+	private double price;
+	
+	@ManyToOne
+    @JoinColumn(name = "categoryid")
+    private Category category;	 //<-----------Tämän pitää olla sama kuin Category.javassa mappedBy = "category"
+	
+	public Book() {}
+	
+	public Book(String title, String author, String isbn, int publicationYear, double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.publicationYear = publicationYear;
+		this.price = price;
+	}
+	public Book(String title, String author, String isbn, int publicationYear, double price, Category category) {
+		super();
+		//this.id = id;
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.publicationYear = publicationYear;
+		this.price = price;
+		this.category = category;
+	}
+	
+	
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+	public void setPublicationYear(int publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	/*
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", isbn=" + isbn + ", publicationYear=" + publicationYear
+				+ ", price=" + price + "]";
+	}
+	*/
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", isbn=" + isbn + ", publicationYear="
+				+ publicationYear + ", price=" + price + "]";
+	}
+	
+	
+	
+	
+	
+
+}
